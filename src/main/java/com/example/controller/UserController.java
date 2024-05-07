e20a5489233965482d45b246e4f2ff9349a87d53
emmanueldavids <emmanueldavids417@gmail.com>
2024-05-07 19:37:31 +0000

    Added Get method and Post method

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,12 +1,34 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.Entity.User;
+import com.example.Repo.UserRepo;
+
+
 
 @RestController
 public class UserController {
-
+    @Autowired
+    UserRepo repo;
+    
     //Get method 2x
+    @GetMapping("/api/users")    
+    public List<User> getAllUser(){
+        List<User> user = repo.findAll();
+        return user;
+    }
 
+    //get method by Id
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id){
+        User user = repo.findById(id).get();
+        return user;
+
+    }
     //Post method
     //Update or put method
     //Delete method
