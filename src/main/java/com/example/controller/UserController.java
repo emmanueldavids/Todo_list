0f371b815342a76bd322069d55a8f715ad802c1e
emmanueldavids <emmanueldavids417@gmail.com>
2024-05-07 19:16:24 +0000

    where the REST API methods CRUD will be

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    //Get method 2x
+
+    //Post method
+    //Update or put method
+    //Delete method
+
+
+}
