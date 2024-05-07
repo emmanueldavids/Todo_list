0620d0055cfc9d57a1c7822a4a339d35fc22d9f1
emmanueldavids <emmanueldavids417@gmail.com>
2024-05-07 19:17:46 +0000

    where the repo will be

---
+++ src/main/java/com/example/Repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.Entity.User;
+
+public class UserRepo extends JpaRepository<User, Integer>{
+
+}
