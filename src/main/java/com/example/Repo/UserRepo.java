a63ff0aca0c3fe9723bce33b730cc896d0173b32
emmanueldavids <emmanueldavids417@gmail.com>
2024-05-08 12:06:25 +0000

    Resolved the JpaRepository error

--- src/main/java/com/example/Repo/UserRepo.java
+++ src/main/java/com/example/Repo/UserRepo.java
@@ -1,9 +1,10 @@
 package com.example.Repo;
 
-import com.example.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.Entity.User;
 
-public class UserRepo extends JpaRepository<User, Integer>{
+
+public interface UserRepo extends JpaRepository<User, Integer>{
 
 }
